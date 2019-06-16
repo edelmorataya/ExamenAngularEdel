447aabb4ec6cf125615de99b659a6c700ccdbbc4
edelmorataya <edelmorataya@us-MacBook-Pro.local>
2019-06-15 22:44:04 -0600

    Add Thumbs

--- app/js/controllers/gallery/galleryController.js
+++ app/js/controllers/gallery/galleryController.js
@@ -8,7 +8,21 @@
       let vm = this;
       vm.slides1 = JSON.parse (localStorage.getItem("products"));
 
-      
+
+      vm.getActiveSlide = function () {
+        var activeSlide = slides.filter(function (s) { 
+          return s.active;
+        })[0];
+        
+        alert(slides.indexOf(activeSlide));
+        
+      };
+
+      vm.setActiveSlide = function (idx) {
+        console.log("runproperly")
+        vm.slides1[idx].active=true;
+      };
+    
 
   });
 
